4a2de35e0e87aa96d20ee2a995540e3219f9c864
17358761487 <2053752041@qq.com>
2025-09-08 14:13:35 +0800

    完成  weightconcert作业

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 weight_input = input()
-unit = weight_input[:-2]
-value = float(weight_input[:-2])
-if unit = 'kg':
-    result = value*2.2046
-    printt(f"对应的英制重量为{result:.3f}磅")
-elif unit = 'pd':
-    result = value/2.2046
+unit = weight_input[-2:] 
+value = float(weight_input[:-2]) 
+if unit == 'kg':
+    result = value * 2.2046
+    print(f"对应的英制重量为{result:.3f}磅")
+elif unit == 'pd':
+    result = value / 2.2046
     print(f"对应的公制重量为{result:.3f}公斤")
 else:
-    print("输入格式错误，请以'kg'或'pd'结尾") 
+    print("输入格式错误，请使用'kg'或'pd'作为单位")
