ebfca2fde1ce1ad1e1f26b6046cb0c3f4b9959f5
17358761487 <2053752041@qq.com>
2025-09-08 14:24:40 +0800

    完成  weightconcert作业

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-if len(weight_input) < 2:
-    print("输入格式错误，请使用'kg'或'lb'作为单位（如10kg、5lb）")
+weight_input = input()
+unit = weight_input[-2:] 
+value = float(weight_input[:-2]) 
+if unit == 'kg':
+    result = value * 2.2046
+    print(f"对应的英制重量为{result:.3f}磅")
+elif unit == 'pd':
+    result = value / 2.2046
+    print(f"对应的公制重量为{result:.3f}公斤")
 else:
-    if weight_input[-2:] == 'kg':
-        try:
-            value = float(weight_input[:-2])
-            result = value * 2.2046
-            print(f"对应的英制重量为{result:.3f}磅")
-        except ValueError:
-            print("数值格式错误，请输入有效的数字")
-    
-    elif weight_input[-2:] == 'pd':
-        try:
-            value = float(weight_input[:-2])
-            result = value / 2.2046
-            print(f"对应的公制重量为{result:.3f}公斤")
-        except ValueError:
-            print("数值格式错误，请输入有效的数字")
-    
-    else:
-        print("输入格式错误，请使用'kg'或'pd'作为单位（如10kg、5lb）")
+    print("输入格式错误，请使用'kg'或'pd'作为单位（如10kg、5pd）")
