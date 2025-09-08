8380f2bf974672edb08367926594c7132512dae8
17358761487 <2053752041@qq.com>
2025-09-08 15:01:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ value = float(weight_input[:-2])
 if unit == 'kg':
     result = value * 2.2046
     print(f"对应的英制重量为{result:.3f}磅")
-else
+if unit == 'pd':
     result = value / 2.2046
     print(f"对应的公制重量为{result:.3f}公斤")
+else:
+    print("输入格式错误，请使用'kg'或'pd'作为单位（如10kg、5pd）")
 
