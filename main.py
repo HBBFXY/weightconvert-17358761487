f89410ae28f682f43ff270275b9e6dc9d6fdbd3e
17358761487 <2053752041@qq.com>
2025-09-08 13:40:55 +0800

    完成  weightconcert作业

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 # 在这个文件下编写代码，题目具体要求见README.md文件
-s = input("请输入重量")
-if s.endswith('kg'):
-    num = float(s[:-2])
-    result = num*2.2046
-    print(f"对应的英制重量为{result:.3f}磅")
-elif s.endswith('pd'):
-    num = float(s[:-2])
-    result = num /2.2046
-    print(f"对应的公制重量为{result:.3f}磅")
-else:
-    print("输入的格式错误，请以kg或pd结尾")    
+def main():
+    s = input("请输入重量")
+    if s.endswith('kg'):
+        num = float(s[:-2])
+        result = num*2.2046
+        print(f"对应的英制重量为{result:.3f}磅")
+    elif s.endswith('pd'):
+        num = float(s[:-2])
+        result = num /2.2046
+        print(f"对应的公制重量为{result:.3f}磅")
+    else:
+        print("输入的格式错误，请以kg或pd结尾")   
+if __name__=="__main__":
+    main()
