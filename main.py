39c69e7a7ca50f11cbbf786b2c315a00e90526cf
17358761487 <2053752041@qq.com>
2025-09-09 13:10:20 +0800

    完成  weightconcert作业

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if unit == 'kg':
     result = value * 2.2046
     print(f"对应的英制重量为{result:.3f}磅")
 elif unit == 'pd':
-    result = value / 2.2046
+    result = value / 2.2049
     print(f"对应的公制重量为{result:.3f}公斤")
 else:
     print("输入格式错误，请使用'kg'或'pd'作为单位（如10kg、5pd）")
